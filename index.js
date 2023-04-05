befe3d99f3d4d35097aa7a5dce4163171419c7da
Pablo Fernandez <pablo.hinojosa@ort.edu.ar>
2023-04-05 01:28:03 +0000

    archivo csv

--- index.js
+++
@@ -1,4 +0,0 @@
-//const chalk = require("chalk");
-import chalk from "chalk";
-
-console.log(chalk.red("Hola mundo en rojo"));
---
+++ src/index.js
@@ -0,0 +1,7 @@
+//const chalk = require("chalk");
+import chalk from "chalk";
+import _ from "lodash";
+
+console.log(chalk.red("Hola mundo en rojo....!"));
+const ejemplo = _.join(["Hola ", "mundo"]);
+console.log(ejemplo);
